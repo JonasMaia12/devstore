db33040c6c2f3ec0322fc255aca2cf0c90e6509e
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-02-04 09:19:43 +0000

    Initial structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: "var(--font-inter)",
+    },
+  },
   plugins: [],
-} satisfies Config;
+} satisfies Config
