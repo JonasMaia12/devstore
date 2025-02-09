b91c69efcd08b9e2883875a651f74aadb518d1c3
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-02-09 10:32:31 +0000

    Add client server/client env variables

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -58,9 +58,9 @@ export default async function Search({ searchParams }: SearchProps) {
               <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
                 <span className="text-sm truncate">{product.title}</span>
                 <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                  {product.price.toLocaleString('pt-BR', {
+                  {product.price.toLocaleString('pt-PT', {
                     style: 'currency',
-                    currency: 'BRL',
+                    currency: 'EUR',
                     minimumFractionDigits: 0,
                     maximumFractionDigits: 0,
                   })}
